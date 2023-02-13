e5ef6e72be1ba3a09d1474c39d81311678b45c4d
swati <s.swati.5293@gmail.com>
2023-02-13 13:04:16 +0530

    13th Feb'23

--- index.js
+++ index.js
@@ -1,35 +1,104 @@
 const express = require('express'); //importing express js
 const path = require('path'); //requiring path module to set up views folder
 const port = 8000; // setting the port
+const db = require('./config/mongoose');//requiring the file(library) for db connection
+const CreateProjectDetails = require('./models/projectDetails');// requiring database schema
+
 const app = express(); //running express 
 
 app.set('view engine', 'ejs'); //setting view engine as ejs
 app.set('views', path.join(__dirname, 'views')); //setting up views folder
 app.use(express.urlencoded());//middleware to parse data
-app.use(express.static('assets'));
+app.use(express.static('assets'));//setting up assets directory
+
 
 
 // use express router
 // app.use('/', require('./routes'));
 
+// var createProj = [
+//     {
+//         name: "Xy",
+//         description: "qwqwe",
+//         author: "swa"
+//     },
+//     {
+//         name: "po",
+//         description: "lk",
+//         author: "swzee"
+//     }  
+// ]
+  
+//home page
 app.get("/", function(req, res){
 
-    return res.render('index', {
-        title: "Home"
+    CreateProjectDetails.find({}, function(err, allProjects){
+        if(err){
+            console.log('Error in fetching the entire project list! ');
+        }
+        return res.render('index', {
+            title: "Home",
+            create_Proj: allProjects
+        });
+
     });
 
 });
 
+//create project page
 app.post('/createProjectDialogue', function(req, res){
     return res.render('createProjectDialogue', {
         title: "Create Project"
     });
 });
 
+//after creating a project it gets displayed in the home page list
 app.post('/create-Project', function(req, res){
-    console.log(req.body);
-    return res.redirect('/');
+    
+    CreateProjectDetails.create({
+        name: req.body.name,
+        description: req.body.description,
+        author: req.body.author
+    }, function(err, newProject){
+        if(err){
+            console.log('error in creating a new project in the database!');
+            return;
+        }
+        return res.redirect('/');
+    });
+
 });
+
+app.get('/projectDetails', function(req,res){
+
+});
+
+
+//this function can delete a new project created(home page)
+app.get('/deleteProjectDetails', function(req,res){
+
+     let id = req.query.id;//get the id from url paramteres for the selected project
+     
+     //find the project by id and delete from the database
+     CreateProjectDetails.findByIdAndDelete(id, function(err){
+        if(err){
+            console.log('error in deleting the project from the DB');
+            return;
+        }
+        return res.redirect('back');
+     });
+});
+
+
+
+
+
+
+
+
+
+
+
 
 
 //verifying if the server is connected or not
--- models/projectDetails.js
+++ models/projectDetails.js
@@ -1,1 +1,20 @@
-const mongoose = require('');
+const mongoose = require('mongoose');
+
+const projectDetails = new mongoose.Schema({ //projectDetails object will create an instance of the schema
+    name: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    author: {
+        type: String,
+        required: true
+    }
+});
+
+
+const CreateProjectDetails = mongoose.model('CreateProjectDetails', projectDetails); //calling model method on mongoose object and pass projectDetais schema abd call it CreateProjectDetials
+module.exports = CreateProjectDetails;
